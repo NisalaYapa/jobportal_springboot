058f9ab24471e9dff3b37b377f2fbca98b78c63e
Nisala Yapa <nisalayapanc@gmail.com>
2025-03-01 12:00:04 +0530

    Security

--- jobportal/src/main/java/com/nisala/jobportal/services/UsersService.java
+++ jobportal/src/main/java/com/nisala/jobportal/services/UsersService.java
@@ -7,6 +7,12 @@ import com.nisala.jobportal.repository.JobSeekerProfileRepository;
 import com.nisala.jobportal.repository.RecruiterProfileRepository;
 import com.nisala.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,20 +23,22 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository , JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository){
+    public UsersService(UsersRepository usersRepository , JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder){
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this. passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
 
-        System.out.println(users.toString());
 
 
         int userTypeId = users.getUserTypeId().getUserTypeId();
@@ -52,5 +60,28 @@ public class UsersService {
 
     public Optional<Users> getUsersByEmail(String email){
         return usersRepository.findByEmail(email);
+    }
+
+    public Object getCurrentUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Couldnot find user"));
+            int userId = users.getUserId();
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                RecruiterProfile recruiterProfile = recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+                return recruiterProfile;
+            }
+            else{
+
+                JobSeekerProfile jobSeekerProfile = jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+
+                return jobSeekerProfile;
+
+            }
+        }
+
+        return null;
     }
 }
